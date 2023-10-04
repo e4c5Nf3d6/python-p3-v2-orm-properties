9304849efe650b5f42dc9ccc587df832af0e5bc0
Maria Jenkins <macjenkin@gmail.com>
2023-10-04 17:12:32 -0500

    complete lab

--- lib/employee.py
+++ lib/employee.py
@@ -5,7 +5,6 @@ from department import Department
 
 class Employee:
 
-    # Dictionary of objects saved to the database.
     all = {}
 
     def __init__(self, name, job_title, department_id, id=None):
@@ -19,10 +18,48 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name):
+            self._name = name
+        else:
+            raise ValueError(
+                "Name must be a non-empty string"
+            )
+        
+    @property
+    def job_title(self):
+        return self._job_title
+
+    @job_title.setter
+    def job_title(self, job_title):
+        if isinstance(job_title, str) and len(job_title):
+            self._job_title = job_title
+        else:
+            raise ValueError(
+                "job_title must be a non-empty string"
+            )
+        
+    @property
+    def department_id(self):
+        return self._department_id
+    
+    @department_id.setter
+    def department_id(self, department_id):
+        if type(department_id) is int and Department.find_by_id(department_id):
+            self._department_id = department_id
+        else:
+            raise ValueError(
+                "department_id must reference a department in the database"
+            )
 
     @classmethod
     def create_table(cls):
-        """ Create a new table to persist the attributes of Employee instances """
         sql = """
             CREATE TABLE IF NOT EXISTS employees (
             id INTEGER PRIMARY KEY,
@@ -36,7 +73,6 @@ class Employee:
 
     @classmethod
     def drop_table(cls):
-        """ Drop the table that persists Employee instances """
         sql = """
             DROP TABLE IF EXISTS employees;
         """
@@ -44,9 +80,6 @@ class Employee:
         CONN.commit()
 
     def save(self):
-        """ Insert a new row with the name, job title, and department id values of the current Employee object.
-        Update object id attribute using the primary key value of new row.
-        Save the object in local dictionary using table row's PK as dictionary key"""
         sql = """
                 INSERT INTO employees (name, job_title, department_id)
                 VALUES (?, ?, ?)
@@ -59,7 +92,6 @@ class Employee:
         type(self).all[self.id] = self
 
     def update(self):
-        """Update the table row corresponding to the current Employee instance."""
         sql = """
             UPDATE employees
             SET name = ?, job_title = ?, department_id = ?
@@ -70,9 +102,6 @@ class Employee:
         CONN.commit()
 
     def delete(self):
-        """Delete the table row corresponding to the current Employee instance,
-        delete the dictionary entry, and reassign id attribute"""
-
         sql = """
             DELETE FROM employees
             WHERE id = ?
@@ -81,32 +110,24 @@ class Employee:
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
 
-        # Delete the dictionary entry using id as the key
         del type(self).all[self.id]
 
-        # Set the id to None
         self.id = None
 
     @classmethod
     def create(cls, name, job_title, department_id):
-        """ Initialize a new Employee instance and save the object to the database """
         employee = cls(name, job_title, department_id)
         employee.save()
         return employee
 
     @classmethod
     def instance_from_db(cls, row):
-        """Return an Employee object having the attribute values from the table row."""
-
-        # Check the dictionary for  existing instance using the row's primary key
         employee = cls.all.get(row[0])
         if employee:
-            # ensure attributes match row values in case local instance was modified
             employee.name = row[1]
             employee.job_title = row[2]
             employee.department_id = row[3]
         else:
-            # not in dictionary, create new instance and add to dictionary
             employee = cls(row[1], row[2], row[3])
             employee.id = row[0]
             cls.all[employee.id] = employee
@@ -114,7 +135,6 @@ class Employee:
 
     @classmethod
     def get_all(cls):
-        """Return a list containing one Employee object per table row"""
         sql = """
             SELECT *
             FROM employees
@@ -126,7 +146,6 @@ class Employee:
 
     @classmethod
     def find_by_id(cls, id):
-        """Return Employee object corresponding to the table row matching the specified primary key"""
         sql = """
             SELECT *
             FROM employees
@@ -138,7 +157,6 @@ class Employee:
 
     @classmethod
     def find_by_name(cls, name):
-        """Return Employee object corresponding to first table row matching specified name"""
         sql = """
             SELECT *
             FROM employees
